adfa925bfd2a1fe03f40975869b98f3c56a6f8b9
Ngọc Huyền <caothingochuyen2609@gmail.com>
2024-10-11 16:40:22 +0700

    #1 finish task hello world

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,12 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    //log
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:2.0.16")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-log4j12
+    implementation("org.slf4j:slf4j-log4j12:2.0.16")
 }
 
 tasks.test {
